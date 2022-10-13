571bbbbf6dfb3ba2d0d281001d9fa043beb0f7da
solextin <106186983+solextin@users.noreply.github.com>
2022-10-12 22:06:15 -0700

    modified settings to make SECRET_KEY environment variable

--- booktime/settings.py
+++ booktime/settings.py
@@ -12,6 +12,10 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 import os
+
+from dotenv import load_dotenv
+load_dotenv()
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-9x_15l2x^xih1h-k(wqd(ev!52q7sezafo=2#ufjc)%v$nt6ja'
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
